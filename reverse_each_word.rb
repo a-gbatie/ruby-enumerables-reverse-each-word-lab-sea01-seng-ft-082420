22939efc1084b19a9da8d30032f594b5233e92aa
Ange Batie <ange.batie@gmail.com>
2020-08-20 00:52:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   array = string.split("")
-  new_array = { |element| new_array << element.reverse }
+  new_array = new_array{ |element| new_array << element.reverse }
   new_array.join("")
 end
